6ecee00e001f62bb5d4756e46ccfc83098fa7c88
Nitisarath Phuttiphaphong <64013165+PluzNtp@users.noreply.github.com>
2022-04-04 20:28:48 +0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,150 +1,143 @@
-import pygame
-import random
-from enum import Enum
-from collections import namedtuple
-
-pygame.init()
-font = pygame.font.Font('arial.ttf', 25)
+import turtle as t
+import os
 
 
+player_a_score = 0
+player_b_score = 0
+
+win = t.Screen()   
+win.title("Ping-Pong Game") 
+win.bgcolor('black')   
+win.setup(width=800,height=600) 
+win.tracer(0)   
 
 
-class Direction(Enum):
-    RIGHT = 1
-    LEFT = 2
-    UP = 3
-    DOWN = 4
+
+paddle_left = t.Turtle()
+paddle_left.speed(0)
+paddle_left.shape('square')
+paddle_left.color('red')
+paddle_left.shapesize(stretch_wid=5,stretch_len=1)
+paddle_left.penup()
+paddle_left.goto(-350,0)
 
 
-Point = namedtuple('Point', 'x, y')
 
-# rgb
-WHITE = (255, 255, 255)
-RED = (200, 0, 0)
-BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
-BLACK = (0, 0, 0)
-
-BLOCK_SIZE = 20
-SPEED = 20
+paddle_right = t.Turtle()
+paddle_right.speed(0)
+paddle_right.shape('square')
+paddle_right.shapesize(stretch_wid=5,stretch_len=1)
+paddle_right.color('red')
+paddle_right.penup()
+paddle_right.goto(350,0)
 
 
-class SnakeGame:
 
-    def __init__(self, w=640, h=480):
-        self.w = w
-        self.h = h
-
-        self.display = pygame.display.set_mode((self.w, self.h))
-        pygame.display.set_caption('Snake')
-        self.clock = pygame.time.Clock()
-
-
-        self.direction = Direction.RIGHT
-
-        self.head = Point(self.w / 2, self.h / 2)
-        self.snake = [self.head,
-                      Point(self.head.x - BLOCK_SIZE, self.head.y),
-                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
-
-        self.score = 0
-        self.food = None
-        self._place_food()
-
-    def _place_food(self):
-        x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
-        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
-        self.food = Point(x, y)
-        if self.food in self.snake:
-            self._place_food()
-
-    def play_step(self):
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.direction = Direction.LEFT
-                elif event.key == pygame.K_RIGHT:
-                    self.direction = Direction.RIGHT
-                elif event.key == pygame.K_UP:
-                    self.direction = Direction.UP
-                elif event.key == pygame.K_DOWN:
-                    self.direction = Direction.DOWN
+ball = t.Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.color('yellow')
+ball.penup()
+ball.goto(0,0)
+ball_dx = 1.5   
+ball_dy = 1.5
 
 
-        self._move(self.direction)
-        self.snake.insert(0, self.head)
+
+pen = t.Turtle()
+pen.speed(0)
+pen.color('skyblue')
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0            Player B: 0 ",align="center",font=('Monaco',24,"normal"))
 
 
-        game_over = False
-        if self._is_collision():
-            game_over = True
-            return game_over, self.score
 
 
-        if self.head == self.food:
-            self.score += 1
-            self._place_food()
-        else:
-            self.snake.pop()
+def paddle_left_up():
+    y = paddle_left.ycor()
+    y = y + 15
+    paddle_left.sety(y)
 
 
-        self._update_ui()
-        self.clock.tick(SPEED)
-
-        return game_over, self.score
-
-    def _is_collision(self):
-
-        if self.head.x > self.w - BLOCK_SIZE or self.head.x < 0 or self.head.y > self.h - BLOCK_SIZE or self.head.y < 0:
-            return True
-        # hits itself
-        if self.head in self.snake[1:]:
-            return True
-
-        return False
-
-    def _update_ui(self):
-        self.display.fill(BLACK)
-
-        for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12))
-
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
-
-        text = font.render("Score: " + str(self.score), True, WHITE)
-        self.display.blit(text, [0, 0])
-        pygame.display.flip()
-
-    def _move(self, direction):
-        x = self.head.x
-        y = self.head.y
-        if direction == Direction.RIGHT:
-            x += BLOCK_SIZE
-        elif direction == Direction.LEFT:
-            x -= BLOCK_SIZE
-        elif direction == Direction.DOWN:
-            y += BLOCK_SIZE
-        elif direction == Direction.UP:
-            y -= BLOCK_SIZE
-
-        self.head = Point(x, y)
+def paddle_left_down():
+    y = paddle_left.ycor()
+    y = y - 15
+    paddle_left.sety(y)
 
 
-if __name__ == '__main__':
-    game = SnakeGame()
 
-    # game loop
-    while True:
-        game_over, score = game.play_step()
+def paddle_right_up():
+    y = paddle_right.ycor()
+    y = y + 15
+    paddle_right.sety(y)
 
-        if game_over == True:
-            break
 
-    print('Final Score', score)
 
-    pygame.quit()
+def paddle_right_down():
+    y = paddle_right.ycor()
+    y = y - 15
+    paddle_right.sety(y)
+
+
+
+win.listen()
+win.onkeypress(paddle_left_up,"u")
+win.onkeypress(paddle_left_down,"e")
+win.onkeypress(paddle_right_up,"Up")
+win.onkeypress(paddle_right_down,"Down")
+
+
+
+
+
+
+while True:
+
+   
+    ball.setx(ball.xcor() + ball_dx)
+    ball.sety(ball.ycor() + ball_dy)
+
+   
+
+    if ball.ycor() > 290:  
+        ball.sety(290)
+        ball_dy = ball_dy * -1
+        
+    
+    if ball.ycor() < -290: 
+        ball.sety(-290)
+        ball_dy = ball_dy * -1
+        
+
+    if ball.xcor() > 390:   
+        ball.goto(0,0)
+        ball_dx = ball_dx * -1
+        player_a_score = player_a_score + 1
+        pen.clear()
+        pen.write("Player A: {}                    Player B: {} ".format(player_a_score,player_b_score),align="center",font=('Monaco',24,"normal"))
+        os.system("afplay wallhit.wav&")
+
+
+
+    if(ball.xcor()) < -390: 
+        ball.goto(0,0)
+        ball_dx = ball_dx * -1
+        player_b_score = player_b_score + 1
+        pen.clear()
+        pen.write("Player A: {}                    Player B: {} ".format(player_a_score,player_b_score),align="center",font=('Monaco',24,"normal"))
+        os.system("afplay wallhit.wav&")
+
+
+    
+
+    if(ball.xcor() > 340) and (ball.xcor() < 350) and (ball.ycor() < paddle_right.ycor() + 40 and ball.ycor() > paddle_right.ycor() - 40):
+        ball.setx(340)
+        ball_dx = ball_dx * -1
+        os.system("afplay paddle.wav&")
+
+    if(ball.xcor() < -340) and (ball.xcor() > -350) and (ball.ycor() < paddle_left.ycor() + 40 and ball.ycor() > paddle_left.ycor() - 40):
+        ball.setx(-340)
+        ball_dx = ball_dx * -1
+        os.system("afplay paddle.wav&")
